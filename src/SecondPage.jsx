7a18619712c7e9c4f9d2c77f7a7dd1f0ef965e96
Avinish Kumar Tripathi <avinishkumartripathi@gmail.com>
2026-08-10 20:10:51 +0530

    C

--- src/SecondPage.jsx
+++ src/SecondPage.jsx
@@ -154,7 +154,7 @@ export default function SecondPage({ onOpenQrSidebar }) {
           {/* Right Column: Title */}
           <div className="second-page-right-col">
             <h2 className="second-page-title">
-              Hear <span className="text-highlight">Namratha</span> tell a story
+              Hear <span className="text-highlight">Namratha</span> tell a Story.
             </h2>
           </div>
         </div>
